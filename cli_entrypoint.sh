9164e46a3542c27f6d8d5cb1a6d7af90106c488f
Sam Dillard <sam@influxdata.com>
2021-08-24 14:52:06 -0700

    fix bug where mounted entrypoint for CLI was wrong path -- dbrp mapping creation now works

--- cli_entrypoint.sh
+++ cli_entrypoint.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 apt-get update
 apt-get install jq -y
 
-# n=0
-# until [ "$n" -ge 25 ]
-# do
-   # influx setup --bucket mqtt -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} -o ${DOCKER_INFLUXDB_INIT_ORG} --username=${DOCKER_INFLUXDB_INIT_USERNAME} --password=${DOCKER_INFLUXDB_INIT_PASSWORD} --host=http://influxd:8086 -f --skip-verify && break  # substitute your command here
-#    influx setup --bucket mqtt -t ${INFLUX_TOKEN} -org ${INFLUX_ORG} --username=${INFLUX_USERNAME} --password=${INFLUX_PASSWORD} --host=http://influxd:8086 -f --skip-verify && break
-#    n=$((n+1)) 
-# done
+while ! curl -s -k http://influxd:8086/health; do sleep 1; done
 
 influx setup --bucket mqtt -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} --org ${DOCKER_INFLUXDB_INIT_ORG} --username=${DOCKER_INFLUXDB_INIT_USERNAME} --password=${DOCKER_INFLUXDB_INIT_PASSWORD} --host=http://influxd:8086 -f --skip-verify
 
@@ -18,7 +12,7 @@ influx task create \
    --token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} \
    -f /average_temp_5m.flux
 
-INFLUX_BUCKET_ID=$( until )
+
 export INFLUX_BUCKET_ID=$(curl --get http://influxd:8086/api/v2/buckets --header "Authorization: Token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN}"  \
 --header "Content-type: application/json" \
 --data-urlencode "org=samhld" | jq .buckets | jq 'map(select(.name=="mqtt")) | .[].id' | tr -d '"')
@@ -30,3 +24,12 @@ influx v1 dbrp create \
    --rp "default" \
    --token ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} \
    --host http://influxd:8086
+
+
+# n=0
+# until [ "$n" -ge 25 ]
+# do
+   # influx setup --bucket mqtt -t ${DOCKER_INFLUXDB_INIT_ADMIN_TOKEN} -o ${DOCKER_INFLUXDB_INIT_ORG} --username=${DOCKER_INFLUXDB_INIT_USERNAME} --password=${DOCKER_INFLUXDB_INIT_PASSWORD} --host=http://influxd:8086 -f --skip-verify && break  # substitute your command here
+#    influx setup --bucket mqtt -t ${INFLUX_TOKEN} -org ${INFLUX_ORG} --username=${INFLUX_USERNAME} --password=${INFLUX_PASSWORD} --host=http://influxd:8086 -f --skip-verify && break
+#    n=$((n+1)) 
+# done
